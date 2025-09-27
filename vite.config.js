0d8c3c2f740795ceaf6929b1c1abbaac990954ab
Bijendra123 <Aapka GitHub Email>
2025-09-27 22:53:31 +0530

    setup for gh-pages deploy

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,8 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 
+// https://vitejs.dev/config/
 export default defineConfig({
-  base: '/Portfolio-/', // 👈 Must match your GitHub repo name exactly
+  base: '/Portfolio-/',   // 👈 Must match your repo name (case + hyphen same)
   plugins: [react()],
-});
+})
